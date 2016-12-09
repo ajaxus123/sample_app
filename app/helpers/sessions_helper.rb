87d3b27a52d5bbfb7b3a6ba7c56cc33905004690
Michael Bekker <bekker.michael2@gmail.com>
2016-12-09 09:10:12 +0000

    Implement advanced login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,7 +7,7 @@ module SessionsHelper
 
     #remembers a user in a persistent session
     def remember(user)
-        user.remember
+    user.remember
         cookies.permanent.signed[:user_id] = user.id
         cookies.permanent[:remember_token] = user.remember_token
     end
@@ -18,12 +18,13 @@ module SessionsHelper
             @current_user ||= User.find_by(id: user_id)
         elsif (user_id = cookies.signed[:user_id])
             user = User.find_by(id: user_id)
-            if user && user.authenticated?(cookies[:remember_token])
+        if user && user.authenticated?(cookies[:remember_token])
             log_in user
-            @current_user = user
-            end
+             @current_user = user
+        end
         end
     end
+        
 
     #returns true if the user is logged in
     def logged_in?
@@ -33,7 +34,7 @@ module SessionsHelper
     
     #forgets a persistent session
     def forget(user)
-        user.forget
+    user.forget
         cookies.delete(:user_id)
         cookies.delete(:remember_token)
     end
@@ -41,8 +42,8 @@ module SessionsHelper
     #logs out user
     def log_out
         forget(current_user)
-       session.delete(:user_id)
-       @current_user = nil
+        session.delete(:user_id)
+        @current_user = nil
     end
 
 end
