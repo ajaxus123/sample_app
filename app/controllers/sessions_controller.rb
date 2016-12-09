2e30521f779feeb26a4f588d2509aee657aa5875
Michael Bekker <bekker.michael2@gmail.com>
2016-12-09 16:43:24 +0000

    11.39

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,11 +6,18 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user && @user.authenticate(params[:session][:password])
+      if @user.activated?
       log_in @user
       params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
       redirect_back_or @user
     else
-      flash.now[:danger] = 'Invalid email/password combination'
+      message = "Account not activated! "
+      message += "Please check your email for the activation link"
+      flash[:warning] = message
+      redirect_to root_path
+      end
+    else
+      flash.now[:danger] = "Invalid email/password combination"
       render 'new'
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -29,15 +29,27 @@ class User < ApplicationRecord
   end
 
   # Returns true if the given token matches the digest.
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
-  end   
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end 
 
     #forgets a user
     def forget
        update_attribute(:remember_digest, nil) 
     end
+  
+  #activates an account
+  def activate
+    update_columns(activated: true, activated_at: Time.zone.now)
+  end
+
+  #Sends activation email
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
 
   private
   
@@ -51,6 +63,8 @@ class User < ApplicationRecord
     self.activation_token = User.new_token
     self.activation_digest = User.digest(activation_token)
   end
+  
+  
 end
 
-
+  
